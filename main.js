680ca709639a1ed296dfca7e3d0a9a3a0fa2c20c
Boris zaidan <Bzmgs31@gmail.com>
2024-08-21 10:51:37 -0300

    update

---
+++ main.js
@@ -0,0 +1,6 @@
+const openModal = () => document.querySelector("#modal").classList.add('active')
+const closeModal = () => document.querySelector("modal").classList.add('remove')
+console.log(openModal)
+
+
+
